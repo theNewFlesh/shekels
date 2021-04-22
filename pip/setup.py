5553d3c25a4aa5b9b6a396fdfc0a773eb0a3336e
Alex Braun <alexander.g.braun@gmail.com>
2021-04-22 20:23:38 +0000

    dropping python 3.6 support until tox works

--- pip/setup.py
+++ pip/setup.py
@@ -40,7 +40,6 @@ web frontend.',
       'Intended Audience :: Developers',
       'License :: OSI Approved :: MIT License',
       'Programming Language :: Python :: 3',
-      'Programming Language :: Python :: 3.6',
       'Programming Language :: Python :: 3.7',
     ],
     extras_require={
