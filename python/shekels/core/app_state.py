2ffdb90c6fae0a1facacf3bc9b685ce7f80455f9
Alex Braun <alexander.g.braun@gmail.com>
2021-08-04 12:51:19 -0700

    WIP

--- python/shekels/core/app_state.py
+++ python/shekels/core/app_state.py
@@ -1,7 +1,9 @@
-from typing import Any, Dict, Optional
+from typing import Any, Optional
 
+from collections import deque
 import datetime
-import json
+
+from lunchbox.enforce import Enforce
 # ------------------------------------------------------------------------------
 
 
@@ -17,105 +19,70 @@ class AppState:
         * percent
         * data
         * timestamp
-
-    Attributes:
-        log_path (str): Path to log file.
-        step (str): Current step of process.
-        message (str): State message.
-        iterator (int): Current iteration in loop.
-        total (int): Total iterations.
-        percent (float): Percent complete.
-        data (object): Data related to state.
-        timestamp (str): Time of construction.
     '''
-    log_path = '/tmp/app_state.json'
+    __instance = None
 
-    def __init__(
-        self, step, message=None, iterator=0, total=None, data=None
-    ):
-        # type: (str, Optional[str], int, Optional[int], Optional[Any]) -> None
+    def __init__(self, memory=100):
+        # type: (int) -> None
         '''
         Constructs an AppState instance.
 
         Args:
-            step (str): Current step of process.
-            message (str, optional): State message. Default: None.
-            iterator (int): Current iteration in loop. Default: 0.
-            total (int, optional): Total iterations. Default: None.
-            data (object, optional): Data related to state. Default: None.
+            memory (int, optional): Maximum number of states to hold in queue.
+                Default: 100.
+
+        Raises:
+            RuntimeError: If attempt to construct duplicate instance. 
+            EnforceError: If memory is less than 1.
         '''
-        iterator += 1
+        if AppState.__instance is not None:
+            msg = 'AppState is a singleton, and so cannot be constructed more '
+            msg += 'than once.'
+            raise RuntimeError(msg)
+        else:
+            AppState.__instance = self
 
-        self.step = step
-        self.iterator = iterator
-        self.total = total
-        self.data = data
-        self.timestamp = datetime.datetime.now().isoformat()
+        msg = 'Memory must be greater or equal to {b}. {a} < {b}.'
+        Enforce(memory, '>=', 1, message=msg)
+        # ----------------------------------------------------------------------
 
-        # create message
-        if message is None:
-            message = '{step}'
-            if iterator is not None and total is not None:
-                message = '{step} - {iterator} of {total}'
-        message = str(message).format(
-            step=step,
-            message=message,
-            iterator=iterator,
-            total=total,
-            percent=self.percent,
-            data=data,
-            timestamp=self.timestamp,
+        state = dict(
+            step='initial state',
+            message=None,
+            iterator=0,
+            total=None,
+            data=None,
         )
-        self.message = message
+        self._queue = deque([state], memory)  # type: deque
 
     @property
-    def percent(self):
-        # type: () -> Optional[float]
+    def state(self):
+        # type: () -> dict
         '''
-        float: Iteration / total.
+        dict: Newest item in queue.
         '''
-        if self.iterator is not None and self.total is not None:
-            return round((self.iterator / float(self.total)) * 100, 2)
-        return None
+        return self._queue[-1]
 
     def __repr__(self):
         # type: () -> str
         '''
         str: String representation of AppState.
         '''
+        state = self.state
         return f'''AppState
-    step: {self.step}
-    message: {self.message}
-    iterator: {self.iterator}
-    total: {self.total}
-    percent: {self.percent}
-    data: {self.data}
-    timestamp: {self.timestamp}
+    step: {state['step']}
+    message: {state['message']}
+    iterator: {state['iterator']}
+    total: {state['total']}
+    percent: {state['percent']}
+    data: {state['data']}
+    timestamp: {state['timestamp']}
 '''[:-1]
 
-    def to_dict(self):
-        # type: () -> Dict[str, Any]
-        '''
-        Converts instance to dictionary.
-
-        Returns:
-            dict: App state dictionary.
-        '''
-        return dict(
-            step=self.step,
-            message=self.message,
-            iterator=self.iterator,
-            total=self.total,
-            percent=self.percent,
-            data=self.data,
-            timestamp=self.timestamp,
-        )
-
-    @staticmethod
-    def log(step, message=None, iterator=0, total=None, data=None):
+    def log(self, step, message=None, iterator=0, total=None, data=None):
         # type: (str, Optional[str], int, Optional[int], Optional[Any]) -> None
         '''
-        Logs state to file.
+        Logs state to queue.
 
         Args:
             step (str): Current step of process.
@@ -124,12 +91,39 @@ class AppState:
             total (int, optional): Total iterations. Default: None.
             data (object, optional): Data related to state. Default: None.
         '''
-        state = AppState(
-            step,
+        iterator += 1
+
+        percent = None
+        if total is not None:
+            percent = round((iterator / float(total)) * 100, 2)
+
+        timestamp = datetime.datetime.now().isoformat()
+
+        # create message
+        if message is None:
+            message = '{step}'
+            if total is not None:
+                message = '{step} - {iterator} of {total}'
+        message = message.format(
+            step=step,
+            message=message,
             iterator=iterator,
             total=total,
-            message=message,
+            percent=percent,
             data=data,
-        ).to_dict()
-        with open(AppState.log_path, 'w') as f:
-            json.dump(state, f, indent=4)
+            timestamp=timestamp,
+        )
+
+        state = dict(
+            step=step,
+            message=message,
+            iterator=iterator,
+            total=total,
+            percent=percent,
+            data=data,
+            timestamp=timestamp,
+        )
+        self._queue.append(state)
+
+
+APP_STATE = AppState(memory=100)
---
+++ python/shekels/server/database_connection.py
@@ -0,0 +1,197 @@
+from typing import Any, Optional
+
+from multiprocessing import Pipe, Pool
+from multiprocessing.managers import BaseManager
+from pprint import pprint, pformat
+import datetime
+import json
+import time
+import traceback
+# ------------------------------------------------------------------------------
+
+
+def error_to_dict(error):
+    args = []  # type: Any
+    for arg in error.args:
+        if hasattr(arg, 'items'):
+            for key, val in arg.items():
+                args.append(pformat({key: pformat(val)}))
+        else:
+            args.append(str(arg))
+    args = ['    ' + x for x in args]
+    args = '\n'.join(args)
+    klass = error.__class__.__name__
+    msg = f'{klass}(\n{args}\n)'
+    return dict(
+        error=error.__class__.__name__,
+        args=list(map(str, error.args)),
+        message=msg,
+        code=500,
+        traceback=traceback.format_exc()
+    )
+
+
+def log_status(
+    process, message=None, iterator=0, total=None, data=None, status='pending'
+):
+    # type: (str, Optional[str], int, Optional[int], Optional[Any], str) -> str
+    '''
+    Create a state log dictionary.
+
+    Args:
+        process (str): Process being logged.
+        message (str, optional): Stateful message. Default: None.
+        iterator (int): Current iteration in loop. Default: None.
+        total (int, optional): Total iterations. Default: None.
+        data (object, optional): Data related to state. Default: None.
+        status (str, optional): Status of process. Default: 'pending'.
+    '''
+    iter_ = iterator + 1
+    percent = None
+    if total is not None:
+        percent = round((iter_ / float(total)) * 100, 2)
+
+    timestamp = datetime.datetime.now().isoformat()
+
+    # create message
+    if message is None:
+        message = '{process}'
+        if total is not None:
+            message = '{process} {status} - {percent}% ({iterator} of {total})'
+    message = message.format(
+        process=process,
+        status=status,
+        message=message,
+        iterator=iter_,
+        total=total,
+        percent=percent,
+        data=data,
+        timestamp=timestamp,
+    )
+
+    log = json.dumps(dict(
+        status=status,
+        process=process,
+        message=message,
+        iterator=iterator,
+        total=total,
+        percent=percent,
+        data=data,
+        timestamp=timestamp,
+    ))
+    return log
+
+
+class Database:
+    def __init__(self, connection, *args, **kwargs):
+        self.connection = connection
+        self.data = []
+
+    def update(self, fail=False):
+        data = []
+        total = 10
+        for i in range(total):
+            time.sleep(0.1)
+            data.append(i)
+            self.connection.send(log_status(
+                'update', iterator=i, total=total
+            ))
+            if fail and i == 5:
+                raise ValueError('Deliberate failure')
+        self.data = data
+        return self
+
+
+class DatabaseConnection:
+    __instance = None
+
+    @staticmethod
+    def _request(connection, database, command, args, kwargs):
+        try:
+            result = getattr(database, command)(*args, **kwargs)
+            connection.send(log_status(command, status='completed'))
+        except Exception as e:
+            connection.send(log_status(
+                command, status='failed', data=error_to_dict(e)
+            ))
+        return result
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(DatabaseConnection)
+        return cls.__instance
+
+    def __init__(self, *args, **kwargs):
+        class DatabaseManager(BaseManager):
+            pass
+
+        attrs = list(filter(lambda x: not x.startswith('_'), dir(Database)))
+        DatabaseManager.register('Database', callable=Database, exposed=attrs)
+        self._manager = DatabaseManager()
+        self._manager.start()
+
+        self._pool = None
+        self._response = None
+        self._parent, self._child = Pipe(duplex=False)
+        self._database = self._manager.Database(self._child, *args, **kwargs)
+        self._child.send(log_status('initialize', status='completed'))
+
+    def request(self, command, *args, **kwargs):
+        self._pool = Pool(1)
+        self._response = self._pool.apply_async(
+            func=DatabaseConnection._request,
+            args=(self._child, self._database, command, args, kwargs),
+        )
+
+    def shutdown(self):
+        self._parent.close()
+        self._child.close()
+        self._manager.shutdown()
+
+    def refresh(self):
+        self._state = json.loads(self._parent.recv())
+
+    @property
+    def response(self):
+        if self.pending:
+            return None
+        if self._pool is not None:
+            self._pool.close()
+            self._pool.join()
+            self._pool.terminate()
+            self._pool = None
+            return self._response.get()
+        return None
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def pending(self):
+        return self.state['status'] == 'pending'
+
+
+DBC = DatabaseConnection(Database, fail=True)
+
+
+def endpoint(event='interval'):
+    if event == 'click':
+        DBC.request('update')
+
+    DBC.refresh()
+    if DBC.pending:
+        return DBC.state['message']
+
+    r = DBC.response
+    if r is not None:
+        return r.data
+    return None
+
+print(endpoint())
+endpoint('click')
+for i in range(10):
+    print(endpoint())
+print(endpoint())
+
+DBC.shutdown()
